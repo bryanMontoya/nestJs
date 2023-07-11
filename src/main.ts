d7f57ba6ffda85730bf9b1a5665cc6e5813db05c
brayanM <lodoco56@gmail.com>
2023-07-10 23:49:16 -0500

    Evitar o alertar parametros incorrectos

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); //Se agrega pipe para validat dtos
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, //Ignora del payload atributos que no esten en el dto.
+    forbidNonWhitelisted: true //Alerta cuando se mandan atributos de mas en el dto.
+  })); //Se agrega pipe para validat dtos
   await app.listen(PORT);
 }
 bootstrap();
