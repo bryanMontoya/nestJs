64937896f063d57c964395e073ce0ef6acbb9480
Bryan Montoya Osorio <42284607+bryanMontoya@users.noreply.github.com>
2023-08-10 21:47:54 +0000

    Prettier

--- src/common/parse-int/parse-int.pipe.ts
+++ src/common/parse-int/parse-int.pipe.ts
@@ -1,12 +1,17 @@
-import { ArgumentMetadata, Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
+import {
+    ArgumentMetadata,
+    Injectable,
+    PipeTransform,
+    BadRequestException,
+} from '@nestjs/common';
 
 @Injectable()
 export class ParseIntPipe implements PipeTransform {
-  transform(value: string, metadata: ArgumentMetadata) {
-    const val = parseInt(value, 10);
-    if (isNaN(val)){
-        throw new BadRequestException(`EL valor #${value} no es un numero`);
+    transform(value: string, metadata: ArgumentMetadata) {
+        const val = parseInt(value, 10);
+        if (isNaN(val)) {
+            throw new BadRequestException(`EL valor #${value} no es un numero`);
+        }
+        return val;
     }
-    return val;
-  }
 }
--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -1,47 +1,60 @@
-import { Controller, Get, Param, Post, Put, Query, Body, HttpCode, HttpStatus,
+import {
+    Controller,
+    Get,
+    Param,
+    Post,
+    Put,
+    Query,
+    Body,
+    HttpCode,
+    HttpStatus,
     //ParseIntPipe
 } from '@nestjs/common';
 
 import { ProductsService } from '../services/products.service';
-import { ParseIntPipe } from '../../common/parse-int/parse-int.pipe'
+import { ParseIntPipe } from '../../common/parse-int/parse-int.pipe';
 import { CreateProductDto, UpdateProductDto } from '../dtos/product.dto';
 @Controller('products')
 export class ProductsController {
-
     //Pipe: Transformar y validar datos.
-    constructor(private productsService: ProductsService){}
+    constructor(private productsService: ProductsService) {}
 
     @Get('filter')
     @HttpCode(HttpStatus.OK)
     getProductFilter() {
-      return  {
-        message: 'Yo soy filter'
-      };
+        return {
+            message: 'Yo soy filter',
+        };
     }
 
     @Get(':productId')
     @HttpCode(HttpStatus.OK)
-    getProduct(@Param('productId', ParseIntPipe) productId: number) { //Todo lo que se recibe por url es string
+    getProduct(@Param('productId', ParseIntPipe) productId: number) {
+        //Todo lo que se recibe por url es string
         return this.productsService.findOne(productId); //ParseIntPipe: Transformación.
     }
 
     @Get()
     @HttpCode(HttpStatus.OK)
     getProductsPagination(
-      @Query('limit') limit = 100,
-      @Query('offset') offset = 0,
-      @Query('brand') brand: string){
+        @Query('limit') limit = 100,
+        @Query('offset') offset = 0,
+        @Query('brand') brand: string,
+    ) {
         return this.productsService.findAll();
     }
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
-    create(@Body() payload: CreateProductDto){
+    create(@Body() payload: CreateProductDto) {
         return this.productsService.create(payload);
     }
 
     @Put(':id')
-    update(@Param('id', ParseIntPipe) id: number, @Body() payload: UpdateProductDto){
+    update(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() payload: UpdateProductDto,
+    ) {
         return this.productsService.update(id, payload);
     }
 }
