b65fac7c5207b137ff61efbb793c579663949673
Bryan Montoya Osorio <42284607+bryanMontoya@users.noreply.github.com>
2023-08-11 02:52:42 +0000

    Use factory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { last, lastValueFrom } from 'rxjs';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
@@ -6,13 +8,24 @@ import { UsersModule } from './users/users.module';
 
 const API_KEY = '12345';
 @Module({
-    imports: [ProductsModule, UsersModule],
+    imports: [ProductsModule, UsersModule, HttpModule],
     controllers: [AppController],
     providers: [
         AppService,
         {
             provide: 'API_KEY',
             useValue: API_KEY,
+        },
+        {
+            provide: 'TASKS',
+            useFactory: async (http: HttpService) => {
+                const request = http.get(
+                    'https://jsonplaceholder.typicode.com/todos',
+                );
+                const tasks = await lastValueFrom(request);
+                return tasks.data;
+            },
+            inject: [HttpService],
         },
     ],
 })
